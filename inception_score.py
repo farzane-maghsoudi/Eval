e14118bc2acca99d37769d6d20d7ac862c3d0175
farzane-maghsoudi <49396097+farzane-maghsoudi@users.noreply.github.com>
2021-08-21 23:32:35 +0430

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -58,6 +58,8 @@ def get_inception_score(images, splits=10):
     assert(type(images) == np.ndarray)
     assert(len(images.shape) == 4)
     assert(images.shape[1] == 3)
+    #print(np.max(images[0]))
+    #print(np.min(images[0]))
     #assert(np.min(images[0]) >= 0 and np.max(images[0]) > 10), 'Image values should be in the range [0, 255]'
     print('Calculating Inception Score with %i images in %i splits' % (images.shape[0], splits))
     start_time=time.time()
