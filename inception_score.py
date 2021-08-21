41155e12abd74df361588d2051c02eb4ba88907a
farzane-maghsoudi <49396097+farzane-maghsoudi@users.noreply.github.com>
2021-08-21 23:06:16 +0430

    Update inception_score.py

--- inception_score.py
+++ inception_score.py
@@ -1,40 +1,47 @@
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+import tensorflow_gan as tfgan
+import os
 import functools
 import numpy as np
 import time
 from tensorflow.python.ops import array_ops
-from tensorflow.python.ops import functional_ops
-from scipy import misc
+# pip install tensorflow-gan
+import tensorflow_gan as tfgan
 import cv2
 from skimage.transform import resize
+session=tf.compat.v1.InteractiveSession()
+# A smaller BATCH_SIZE reduces GPU memory usage, but at the cost of a slight slowdown
+BATCH_SIZE = 64
+INCEPTION_TFHUB = 'https://tfhub.dev/tensorflow/tfgan/eval/inception/1'
+INCEPTION_OUTPUT = 'logits'
 
-import tensorflow_gan as tfgan
-
-#tfgan = tf.contrib.gan
-session = tf.compat.v1.InteractiveSession()
-
-def inception_logits(images, num_splits = 1):
+# Run images through Inception.
+inception_images = tf.compat.v1.placeholder(tf.float32, [None, 3, None, None], name = 'inception_images')
+def inception_logits(images = inception_images, num_splits = 1):
     images = tf.transpose(images, [0, 2, 3, 1])
     size = 299
-    images = tf.image.resize(images, [size, size], method='bilinear')
-    
+    images = tf.compat.v1.image.resize_bilinear(images, [size, size])
     generated_images_list = array_ops.split(images, num_or_size_splits = num_splits)
-    logits = functional_ops.map_fn(
-        fn = functools.partial(tfgan.eval.run_inception, output_tensor = 'logits:0'),
+    logits = tf.map_fn(
+        fn = tfgan.eval.classifier_fn_from_tfhub(INCEPTION_TFHUB, INCEPTION_OUTPUT, True),
         elems = array_ops.stack(generated_images_list),
-        parallel_iterations = 1,
+        parallel_iterations = 8,
         back_prop = False,
         swap_memory = True,
         name = 'RunClassifier')
     logits = array_ops.concat(array_ops.unstack(logits), 0)
     return logits
 
-def get_inception_probs(batch_size, images, inception_images, logits):
-    n_batches = len(images) // batch_size
-    preds = np.zeros([n_batches * batch_size, 1000], dtype = np.float32)
+logits=inception_logits()
+
+def get_inception_probs(inps):
+    session=tf.get_default_session()
+    n_batches = int(np.ceil(float(inps.shape[0]) / BATCH_SIZE))
+    preds = np.zeros([inps.shape[0], 1000], dtype = np.float32)
     for i in range(n_batches):
-        inp = images[i * batch_size:(i + 1) * batch_size] / 255. * 2 - 1
-        preds[i * batch_size:(i + 1) * batch_size] = logits.eval({inception_images:inp})[:, :1000]
+        inp = inps[i * BATCH_SIZE:(i + 1) * BATCH_SIZE] / 255. * 2 - 1
+        preds[i * BATCH_SIZE : i * BATCH_SIZE + min(BATCH_SIZE, inp.shape[0])] = session.run(logits,{inception_images: inp})[:, :1000]
     preds = np.exp(preds) / np.sum(np.exp(preds), 1, keepdims=True)
     return preds
 
@@ -47,19 +54,19 @@ def preds2score(preds, splits=10):
         scores.append(np.exp(kl))
     return np.mean(scores), np.std(scores)
 
-def get_inception_score(batch_size, images, inception_images, logits, splits=10):
+def get_inception_score(images, splits=10):
     assert(type(images) == np.ndarray)
     assert(len(images.shape) == 4)
     assert(images.shape[1] == 3)
     assert(np.min(images[0]) >= 0 and np.max(images[0]) > 10), 'Image values should be in the range [0, 255]'
     print('Calculating Inception Score with %i images in %i splits' % (images.shape[0], splits))
     start_time=time.time()
-    preds = get_inception_probs(batch_size, images, inception_images, logits)
+    preds = get_inception_probs(images)
     mean, std = preds2score(preds, splits)
     print('Inception Score calculation time: %f s' % (time.time() - start_time))
-    return mean, std  # Reference values: 11.34 for 49984 CIFAR-10 training set images, or mean=11.31, std=0.08 if in 10 splits.
+    return mean, std  # Reference values: 11.38 for 50000 CIFAR-10 training set images, or mean=11.31, std=0.10 if in 10 splits.
 
 def get_images(filename):
     x = cv2.imread(filename)
-    x = resize(x, output_shape=(299, 299))
+    x = resize(x, output_shape=(299,299))
     return x
